df48d684d9809b91488bbb38a8659c6bbb8b5a5c
Beatriz <biaberaldo@outlook.com>
2022-07-13 21:00:51 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Navbar from './components/Navbar';
+import { AuthContextProvider } from './context/AuthContext';
+import Login from './pages/login';
+import Signup from './pages/signup';
+import Account from './pages/account';
 
 function App() {
 	return (
 		<>
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-			</Routes>
+			<AuthContextProvider>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route path="/account" element={<Account />} />
+				</Routes>
+			</AuthContextProvider>
 		</>
 	);
 }
