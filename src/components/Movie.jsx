15e3566f92caeb7eb3ec23cd6fbc1695945e2570
Beatriz <biaberaldo@outlook.com>
2022-07-16 17:49:00 -0300

    update

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,8 +1,31 @@
 import React, { useState } from 'react';
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import { UserAuth } from '../context/AuthContext';
+import { db } from '../firebase';
+import { arrayUnion, doc, updateDoc } from 'firebase/firestore';
 
 const Movie = ({ item }) => {
 	const [like, setLike] = useState(false);
+	const [saved, setSaved] = useState(false);
+	const { user } = UserAuth();
+
+	const movieID = doc(db, 'users', `${user?.email}`);
+
+	const saveShow = async () => {
+		if (user?.email) {
+			setLike(!like);
+			setSaved(true);
+			await updateDoc(movieID, {
+				savedShows: arrayUnion({
+					id: item.id,
+					title: item.title,
+					img: item.backdrop_path,
+				}),
+			});
+		} else {
+			alert('Por favor, faça login para favoritar!');
+		}
+	};
 
 	return (
 		<>
@@ -16,7 +39,7 @@ const Movie = ({ item }) => {
 					<p className="white-space-normal text-sm md:text-sm font-bold flex justify-center items-center h-full text-center">
 						{item?.title}
 					</p>
-					<p>
+					<p onClick={saveShow}>
 						{like ? (
 							<FaHeart className="absolute top-4 left-2 text-gray-300" />
 						) : (
--- src/pages/account.jsx
+++ src/pages/account.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import SavedShows from '../components/SavedShows';
 
 const Account = () => {
-	return <div>Account</div>;
+	return (
+		<>
+			<div className="w-full text-white">
+				<img
+					className="w-full h-[400px] object-cover"
+					src="/img/bg/login.jpg"
+					alt="/"
+				/>
+				<div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
+				<div className="absolute top-[20%] p-4 md:p-8">
+					<h1 className="text-3xl md:text-5xl font-bold">
+						Minha Lista
+					</h1>
+				</div>
+			</div>
+			<SavedShows />
+		</>
+	);
 };
 
 export default Account;
